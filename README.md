e7079a01d6e0546e78d555b6d71717abd5c1c74a
Cold Darkness <Darkness@Drak.com>
2019-11-21 00:21:00 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Goal
+Many Hiring Platforms (Ex. Indeed, Intershala, Stackoverflow) Dont allow filtering results by the aspects like :
+1. Salary/Stipend(For interships)
+
+Therefore I made this extremely simple scraping tool that allows one find relevant jobs. Unfortunately The day these websites change their DOM structure this tool will need an update
+
+At This Point It only supports Intershala. In future I'll add more websites
+
+
+## Installation
+You will need python and pip. install dependencies :
+```bash
+pip3 install --trusted-host pypi.python.org -r requirements.txt
+```
+
+## Usage
+Pretty darn simple :
+```bash
+cd hunter; python3.6 main.py
+```
+
+Then one must enter whether they want a remote job or all jobs && also set The Lower Limit of Salary/Stipend
+
+
+## Future
+Add More Websites && A SQLite Database to keep track of which jobs you have already applied for
---
+++ main.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3.6
+global CONST
+CONST = "https://internshala.com"
+import requests, sys, time
+
+# Take User Input
+mode = input("Enter Mode:")
+price = int(input("Enter Lower Limit:"))
+type = "internship"
+if mode :
+    mode = "work-from-home-"
+    type = "jobs"
+if not price : price = 5000
+
+try:
+    for i in range(10):
+        URL = f"{CONST}/internships/{mode}computer%20science-{type}/page-{i+1}"
+        # print(f"\n\n{URL}\n\n")
+        r = requests.get(URL)
+        from bs4 import BeautifulSoup
+        soup = BeautifulSoup(r.text, "html5lib")
+        internships = soup.find_all(class_='individual_internship')
+        for internship in internships:
+            meta = internship.find(class_="stipend_container_table_cell").text.split()
+            range = meta[0].split('-')
+            try:
+                money = int(range[0])
+            except Exception as e:
+                money  = 0
+
+            if price <= money:
+                name = internship.find(class_="link_display_like_text").text.split()
+                link = CONST + internship.find(class_="view_detail_button")["href"]
+                print(name, link, range)
+
+except Exception as e:
+    print("No More Pages Left. Exiting....")
+    exit(0)
+
+"""
+from the block of internships:
+    Take details link if : if stipend exxecds or equals lower-limit
+"""
