669ca97c82b635c48be9f5a4d468c1b7ae9352ec
CosmicSage <prabhakarmishra9@yahoo.com>
2020-04-15 20:05:33 +0530

    fixed false internship magnitude and log support

--- main.py
+++ main.py
@@ -3,15 +3,16 @@ global CONST
 CONST = "https://internshala.com"
 import requests, sys, time
 
+def incentives(meta):
+    if '+' in meta: return meta[meta.index('+') + 1]
+    return ''
+
 def compare(range, price):
     try:
         if len(range) == 1 and price <= int(range[0]): return True
         elif price <= int(range[0]) or price <= int(range[1]): return True
-    except Exception as e:
-        return False
-def incentives(meta):
-    if '+' in meta: return meta[meta.index('+') + 1]
-    return ''
+    except Exception as e: return False
+
 # Take User Input
 mode = input("Enter Mode:")
 price = int(input("Enter Lower Limit:"))
@@ -23,14 +24,16 @@ if mode :
 
 if not price : price = 5000
 
-try:
-    for i in range(25):
+for i in range(25):
+    try:
         URL = f"{CONST}/internships/{mode}computer%20science-{type}/page-{i+1}"
-        # print(f"\n\n{URL}\n\n")
+        print(f"\n\n{URL}\n{i}\n")
+
         r = requests.get(URL)
         from bs4 import BeautifulSoup
         soup = BeautifulSoup(r.text, "html5lib")
         internships = soup.find_all(class_='individual_internship')
+
         for internship in internships:
             meta = internship.find(class_="stipend_container_table_cell").text.split()
             range = meta[0].split('-')
@@ -40,10 +43,11 @@ try:
                 link = CONST + internship.find(class_="view_detail_button")["href"]
                 print(name, link, incentives(meta), range)
 
-except Exception as e:
-    print("No More Pages Left. Exiting....")
-    exit(0)
-
+    except Exception as e:
+        from datetime import datetime
+        with open('./logs/error.log', 'a') as target:
+            target.write(f"{datetime.now().strftime('%H:%M:%S %b %d, %Y')}\n{internship}:{name}\n")
+exit(0)
 """
 from the block of internships:
     Take details link if : if stipend exxecds or equals lower-limit
